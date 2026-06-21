b59c0e5c2ea7eef2fab22e6d6a6fc5f65aedfd81
root <root@localhost>
2026-06-21 21:06:07 +0700

    Cloud Build Request - 2026-06-21 21:06:07

--- app/src/main/kotlin/com/v2ray/ang/extension/_Ext.kt
+++ app/src/main/kotlin/com/v2ray/ang/extension/_Ext.kt
@@ -9,31 +9,19 @@ import me.dozen.dpreference.DPreference
 import org.json.JSONObject
 import java.net.URLConnection
 
-/**
- * Some extensions
- */
-
 val Context.v2RayApplication: AngApplication
     get() = applicationContext as AngApplication
 
-// Usage note: DPreference use Android ContentProvider to redirect multi process access to main process.
-// Currently, RunSoLibV2RayDaemon process will run proxy core, keep minimum configuration and long running
-// in the background, support toggle on/off. That means it should NOT use DPreference after the initial
-// creation and setup of the service
 val Context.defaultDPreference: DPreference
     get() = v2RayApplication.defaultDPreference
 
 inline fun Context.toast(message: Int): Toast = ToastCompat
-        .makeText(this, message, Toast.LENGTH_SHORT)
-        .apply {
-            show()
-        }
+    .makeText(this, message, Toast.LENGTH_SHORT)
+    .apply { show() }
 
 inline fun Context.toast(message: CharSequence): Toast = ToastCompat
-        .makeText(this, message, Toast.LENGTH_SHORT)
-        .apply {
-            show()
-        }
+    .makeText(this, message, Toast.LENGTH_SHORT)
+    .apply { show() }
 
 fun JSONObject.putOpt(pair: Pair<String, Any>) = putOpt(pair.first, pair.second)!!
 fun JSONObject.putOpt(pairs: Map<String, Any>) = pairs.forEach { putOpt(it.key to it.value) }
@@ -44,40 +32,24 @@ const val divisor = 1024F
 fun Long.toSpeedString() = toTrafficString() + "/s"
 
 fun Long.toTrafficString(): String {
-    if (this == 0L)
-        return "\t\t\t0\t  B"
-
-    if (this < threshold)
-        return "${this.toFloat().toShortString()}\t  B"
-
+    if (this == 0L) return "\t\t\t0\t  B"
+    if (this < threshold) return "${this.toFloat().toShortString()}\t  B"
     val kib = this / divisor
-    if (kib < threshold)
-        return "${kib.toShortString()}\t KB"
-
+    if (kib < threshold) return "${kib.toShortString()}\t KB"
     val mib = kib / divisor
-    if (mib < threshold)
-        return "${mib.toShortString()}\t MB"
-
+    if (mib < threshold) return "${mib.toShortString()}\t MB"
     val gib = mib / divisor
-    if (gib < threshold)
-        return "${gib.toShortString()}\t GB"
-
+    if (gib < threshold) return "${gib.toShortString()}\t GB"
     val tib = gib / divisor
-    if (tib < threshold)
-        return "${tib.toShortString()}\t TB"
-
+    if (tib < threshold) return "${tib.toShortString()}\t TB"
     val pib = tib / divisor
-    if (pib < threshold)
-        return "${pib.toShortString()}\t PB"
-
+    if (pib < threshold) return "${pib.toShortString()}\t PB"
     return "∞"
 }
 
 private fun Float.toShortString(): String {
     val s = "%.2f".format(this)
-    if (s.length <= 4)
-        return s
-    return s.substring(0, 4).removeSuffix(".")
+    return if (s.length <= 4) s else s.substring(0, 4).removeSuffix(".")
 }
 
 val URLConnection.responseLength: Long
